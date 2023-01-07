13b738d497b26103b9c650e44c58cca394acc6c3
Ekaterina <ekaterina.shnaygina02@mail.ru>
2023-01-07 16:10:21 +0300

    Определение выходного дня

---
+++ (2)_HomeWork_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трёхзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int length = a.ToString().Length;
+
+while (length != 3)
+{
+    Console.WriteLine ("Вы ввели не трёхзначное число.");
+    Console.Write("Повторите ввод: ");
+    a = Convert.ToInt32(Console.ReadLine());
+    length = a.ToString().Length;
+}
+
+int digit2 = a/10 - a/100*10;
+Console.WriteLine ("Вторая цифрав числе: " + digit2);
---
+++ (2)_HomeWork_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int length = a.ToString().Length;
+
+
+if (length==1 || length==2)
+{
+    Console.WriteLine ("Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine(length[3]);
+}
