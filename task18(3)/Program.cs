324735a4aa80efbd4679f6d4f3ded5120cfce2b7
Ekaterina <ekaterina.shnaygina02@mail.ru>
2023-01-11 16:16:51 +0300

    Определение диапазона координат по заданной четверти

---
+++ task18(3)/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.Write("Введите номер четверти координат: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a<5 && a>0)
+{
+    if (a == 1)
+    {
+        Console.WriteLine("x > 0, y > 0");
+    }
+    if (a == 2)
+    {
+        Console.WriteLine("x < 0, y > 0");
+    }
+    if (a == 3)
+    {
+        Console.WriteLine("x < 0, y < 0");
+    }
+    if (a == 4)
+    {
+      Console.WriteLine("x > 0, y < 0");
+    }
+}
+else
+{ 
+    Console.WriteLine("Такой четверти не существует");
+}
