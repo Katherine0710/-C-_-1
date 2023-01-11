839119fbe02c82f96ac9731f5285d5ebab2226b4
Ekaterina <ekaterina.shnaygina02@mail.ru>
2023-01-11 16:31:12 +0300

    Определение диапазона координат по заданной четверти - массив string

---
+++ task18.2(3)/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+string[] quaters = {"x > 0, y > 0", 
+                    "x < 0, y > 0", 
+                    "x < 0, y < 0", 
+                    "x > 0, y < 0"};
+Console.Write("Введите номер четверти координат: ");
+int quater = Convert.ToInt32(Console.ReadLine());
+
+if (quater >= 1 && quater <= 4)
+{
+    Console.WriteLine(quaters[quater - 1]);
+}
+else
+{ 
+    Console.WriteLine("Такой четверти не существует");
+}
