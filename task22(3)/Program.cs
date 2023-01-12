09ebf862771a4ed86741fd0336e4c8c1b164f793
Ekaterina <ekaterina.shnaygina02@mail.ru>
2023-01-12 06:41:22 +0300

    Выведение квадратов чисел от 1 до N

---
+++ task22(3)/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+double X = N - 1;
+double numb = N - X;
+double square;
+
+while (X >= 0)
+{
+    numb = N - X;
+    square = Math.Pow(numb, 2);
+    Console.Write(square + " ");
+    X = X-1;
+}
